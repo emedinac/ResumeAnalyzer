4fc232d82c42464d5d58be7bd82bab824616de56
eme <edgar.medina@qualityminds.de>
2025-07-09 23:51:09 +0200

    Trying ThreadPoolExecutor

--- src/loader.py
+++ src/loader.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 from datasets import load_dataset
 from langchain_community.vectorstores import FAISS
 from langchain_community.vectorstores import Chroma
@@ -31,50 +32,56 @@ class ResumeLoader:
             sentence_split_regex=r"(?<=[.?!])\s+",  # .?!
         )
 
-    def compute_embeddings(self, split="train", batch_size=1024, vectorstore_type="faiss"):
+    def compute_embeddings(self, split="train", batch_size=1024, num_workers=16):
         # Compute embeddings without chunks
+        self.chunks = {}
         fields = list(self.dataset[split].features.keys())
         for field in fields:
             if "label" == field:
                 continue
-            self.chunks = []
-            batches = (len(self.dataset[split]) // batch_size)+1
-            for i in tqdm(range(batches)):
-                samples = self.dataset[split][field][i *
-                                                     batch_size:
-                                                     (i+1) *
-                                                     batch_size]
-                batch_chunks = self.chunker.create_documents(samples)
-                self.chunks.extend(batch_chunks)
-            print(f"Total chunks: {len(self.chunks)}")
+            samples = self.dataset[split][field]
 
-            self.vectorstore_type = vectorstore_type.lower()
-            if self.vectorstore_type == "faiss":
-                self.vector_db = FAISS.from_documents(self.chunks,
-                                                      self.embedding_model
+            with ThreadPoolExecutor(max_workers=num_workers) as executor:
+                futures = [
+                    executor.submit(self.chunker.create_documents,
+                                    samples[i:i + batch_size])
+                    for i in range(0, len(samples), batch_size)
+                ]
+                chunks = [future.result() for future in futures]
+            # chunks = self.chunker.create_documents(samples)
+            print(f"Total chunks: {len(chunks)}")
+            self.chunks[field] = chunks
+
+    def build_vectorstore(self, vectorstore_type="faiss"):
+        self.vectorstore_type = vectorstore_type
+        fields = list(self.chunks.keys())
+        for field in fields:
+            if vectorstore_type == "faiss":
+                self.vector_db = FAISS.from_documents(self.chunks[field],
+                                                      self.embedding_model,
                                                       )
-            elif self.vectorstore_type == "chroma":
-                self.vector_db = Chroma.from_documents(self.chunks,
-                                                       self.embedding_model
+            elif vectorstore_type == "chroma":
+                self.vector_db = Chroma.from_documents(self.chunks[field],
+                                                       self.embedding_model,
                                                        )
             else:
                 raise ValueError(
-                    f"Unknown vectorstore: {self.vectorstore_type}")
+                    f"Unknown vectorstore: {vectorstore_type}")
 
-    def load_embeddings_faiss(self, loade_path="embeddings"):
+    def load_indexes(self, vectorstore_type="faiss", loade_path="embeddings"):
         load_path = Path(loade_path)
-        self.vector_db = FAISS.load_local(str(load_path),
-                                          self.embedding_model,
-                                          allow_dangerous_deserialization=True
-                                          )
+        if vectorstore_type == "faiss":
+            self.vector_db = FAISS.load_local(str(load_path),
+                                              self.embedding_model,
+                                              allow_dangerous_deserialization=True
+                                              )
+        elif vectorstore_type == "chroma":
+            self.vector_db = Chroma(persist_directory=loade_path,
+                                    embedding_function=self.embedding_model,
+                                    )
         return self.vector_db
 
-    def load_vectors(self, loade_path="embeddings"):
-        return Chroma(persist_directory=loade_path,
-                      embedding_function=self.embedding_model,
-                      )
-
-    def save_index(self, split="train", save_path="embeddings"):
+    def save_indexes(self, split="train", save_path="embeddings"):
         if self.vectorstore_type == "faiss":
             save_path = Path(save_path).joinpath(self.vectorstore_type, split)
             save_path.mkdir(parents=True, exist_ok=True)
@@ -91,13 +98,15 @@ if __name__ == "__main__":
     for split in ["train", "test"]:
         print(f"Processing split: {split}")
         if Path("embeddings/{split}").exists():
-            dataset.load_embeddings_faiss(split)
+            dataset.load_indexes(split)
             print("Embeddings loaded successfully.")
         else:
-            dataset.compute_embeddings(split, "faiss")
-            dataset.save_index(split)
-            dataset.compute_embeddings(split, "chroma")
-            dataset.save_index(split)
+            dataset.compute_embeddings(split)
+            dataset.build_vectorstore(vectorstore_type="faiss")
+            dataset.save_indexes(split)
+            dataset.compute_embeddings(split)
+            dataset.build_vectorstore(vectorstore_type="chroma")
+            dataset.save_indexes(split)
             print("Embeddings computed and saved successfully.")
 
     print("Embeddings computed and saved successfully.")
