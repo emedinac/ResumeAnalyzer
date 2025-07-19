88c05521b21acc1d85aaf7619e3d9fab493d1ff7
eme <edgar.medina@qualityminds.de>
2025-07-19 14:40:43 +0200

    code refactor

--- src/generator/db_generator.py
+++ src/generator/db_generator.py
@@ -77,83 +77,19 @@ class ResumeJobMatchGenerator:
         self.dbs = [db1, db2, db3, db4]
 
         # LLM for JOB Generation
-        model_name = llm_model_names[0]
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        tokenizer.pad_token = tokenizer.eos_token
-        cv_pipe = pipeline(
-            "text-generation",
-            batch_size=32,
-            model=model_name,
-            tokenizer=tokenizer,
-            temperature=0.5,
-            top_p=0.95,
-            return_full_text=False
-        )
-        generator1 = HuggingFacePipeline(pipeline=cv_pipe)
-
-        model_name = "meta-llama/Llama-4-Scout-17B-16E-Instruct"
-        model_name = llm_model_names[1]
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        tokenizer.pad_token = tokenizer.eos_token
-        cv_pipe = pipeline(
-            "text-generation",
-            model=model_name,
-            batch_size=32,
-            tokenizer=tokenizer,
-            temperature=0.5,
-            top_p=0.95,
-            return_full_text=False
-        )
-        generator2 = HuggingFacePipeline(pipeline=cv_pipe)
-
-        model_name = llm_model_names[2]
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        tokenizer.pad_token = tokenizer.eos_token
-        cv_pipe = pipeline(
-            "text-generation",
-            model=model_name,
-            batch_size=32,
-            tokenizer=tokenizer,
-            temperature=0.5,
-            top_p=0.95,
-            return_full_text=False
-        )
-        generator3 = HuggingFacePipeline(pipeline=cv_pipe)
-
-        model_name = llm_model_names[3]
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        tokenizer.pad_token = tokenizer.eos_token
-        cv_pipe = pipeline(
-            "text-generation",
-            model=model_name,
-            batch_size=32,
-            tokenizer=tokenizer,
-            temperature=0.5,
-            top_p=0.95,
-            return_full_text=False
-        )
-        generator4 = HuggingFacePipeline(pipeline=cv_pipe)
-
-        model_name = llm_model_names[4]
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-        tokenizer.pad_token = tokenizer.eos_token
-        cv_pipe = pipeline(
-            "text-generation",
-            model=model_name,
-            batch_size=32,
-            tokenizer=tokenizer,
-            temperature=0.5,
-            top_p=0.95,
-            return_full_text=False
-        )
-        generator5 = HuggingFacePipeline(pipeline=cv_pipe)
-
-        self.generators = [generator1,
-                           generator2,
-                           generator3,
-                           generator4,
-                           generator5
-                           ]
+        for model_name in llm_model_names:
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+            tokenizer.pad_token = tokenizer.eos_token
+            cv_pipe = pipeline(
+                "text-generation",
+                model=model_name,
+                batch_size=4,
+                tokenizer=tokenizer,
+                temperature=0.5,
+                top_p=0.95,
+                return_full_text=False
+            )
+            self.generators.append(HuggingFacePipeline(pipeline=cv_pipe))
 
         # BIAS MODELS :)
         bias_model = "maximuspowers/bias-detection-ner"
